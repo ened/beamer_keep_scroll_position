0057fa92e9d95b54123d5b705d31b04b9177ca5d
Sebastian Roth <sebastian.roth@gmail.com>
2021-04-29 21:01:46 +0100

    Few responsive tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:beamer/beamer.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   final delegate = BeamerRouterDelegate(
+    listener: (state, location) {
+      print(state.uri);
+    },
     locationBuilder: SimpleLocationBuilder(routes: {
-      '/': (context) => ListScreen(),
+      '/': (context) {
+        return ListWrapper();
+      },
       '/detail/:index': (context) {
         final index = context.currentBeamLocation.state.pathParameters['index'];
         return DetailScreen(index: index!);
       },
-      '/query': (context) {
+      '/detail/:index/final': (context) {
+        final index = context.currentBeamLocation.state.pathParameters['index'];
+        return FinalScreen(index: index!);
+      },
+      '/final': (context) {
         final index =
             context.currentBeamLocation.state.queryParameters['index'];
-        return DetailScreen(index: index!);
-      },
-      '/final': (context) => FinalScreen()
+        return FinalScreen(index: index!);
+      }
     }),
   );
 
@@ -32,7 +41,59 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class ListWrapper extends StatefulWidget {
+  @override
+  _ListWrapperState createState() => _ListWrapperState();
+}
+
+class _ListWrapperState extends State<ListWrapper> {
+  String? _index;
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenTypeLayout.builder(
+      mobile: (_) => ListScreen(
+        onSelected: (index) {
+          context.beamToNamed(
+            '/detail/$index',
+            popBeamLocationOnPop: true,
+          );
+        },
+      ),
+      tablet: (_) {
+        return Row(
+          children: [
+            Flexible(
+                flex: 1,
+                child: ListScreen(
+                  onSelected: (index) {
+                    setState(() {
+                      _index = index;
+                    });
+                  },
+                )),
+            if (_index != null)
+              Flexible(
+                flex: 3,
+                child: DetailScreen(index: _index!),
+              )
+          ],
+        );
+      },
+    );
+  }
+}
+
+typedef StringCallback = Function(String);
+
 class ListScreen extends StatelessWidget {
+  const ListScreen({
+    Key? key,
+    required this.onSelected,
+  }) : super(key: key);
+
+  final StringCallback onSelected;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,14 +101,7 @@ class ListScreen extends StatelessWidget {
       body: ListView.builder(
         itemBuilder: (context, index) => ListTile(
           title: Text('Item: $index'),
-          onTap: () => context.beamToNamed(
-            '/detail/$index',
-            popBeamLocationOnPop: true,
-          ),
-          onLongPress: () => context.beamToNamed(
-            '/query?index=$index',
-            popBeamLocationOnPop: true,
-          ),
+          onTap: () => this.onSelected('$index'),
         ),
         itemCount: 1000,
       ),
@@ -72,11 +126,23 @@ class DetailScreen extends StatelessWidget {
             SizedBox(height: 24),
             ElevatedButton(
               onPressed: () {
-                context.beamToNamed(
-                  '/final',
-                  popBeamLocationOnPop: true,
-                  beamBackOnPop: true,
+                final nested = getValueForScreenType<bool>(
+                  context: context,
+                  mobile: true,
+                  tablet: false,
                 );
+                if (nested) {
+                  context.beamToNamed(
+                    '/detail/$index/final',
+                    popBeamLocationOnPop: true,
+                    beamBackOnPop: true,
+                  );
+                } else {
+                  context.beamToNamed(
+                    '/final?index=$index',
+                    popBeamLocationOnPop: true,
+                  );
+                }
               },
               child: Text('to final page'),
             )
@@ -88,6 +154,10 @@ class DetailScreen extends StatelessWidget {
 }
 
 class FinalScreen extends StatelessWidget {
+  const FinalScreen({Key? key, required this.index}) : super(key: key);
+
+  final String index;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
