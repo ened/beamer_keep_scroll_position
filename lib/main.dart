0b43b17227a42a0a2c2c2c1e2c2b124419baa289
Sebastian Roth <sebastian.roth@gmail.com>
2021-04-29 20:01:16 +0100

    push/pop test

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,16 @@ class MyApp extends StatelessWidget {
   final delegate = BeamerRouterDelegate(
     locationBuilder: SimpleLocationBuilder(routes: {
       '/': (context) => ListScreen(),
-      '/detail/:index': (context) => DetailScreen(),
+      '/detail/:index': (context) {
+        final index = context.currentBeamLocation.state.pathParameters['index'];
+        return DetailScreen(index: index!);
+      },
+      '/query': (context) {
+        final index =
+            context.currentBeamLocation.state.queryParameters['index'];
+        return DetailScreen(index: index!);
+      },
+      '/final': (context) => FinalScreen()
     }),
   );
 
@@ -31,8 +40,14 @@ class ListScreen extends StatelessWidget {
       body: ListView.builder(
         itemBuilder: (context, index) => ListTile(
           title: Text('Item: $index'),
-          onTap: () =>
-              context.beamToNamed('/detail/$index', popBeamLocationOnPop: true),
+          onTap: () => context.beamToNamed(
+            '/detail/$index',
+            popBeamLocationOnPop: true,
+          ),
+          onLongPress: () => context.beamToNamed(
+            '/query?index=$index',
+            popBeamLocationOnPop: true,
+          ),
         ),
         itemCount: 1000,
       ),
@@ -41,12 +56,43 @@ class ListScreen extends StatelessWidget {
 }
 
 class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key, required this.index}) : super(key: key);
+
+  final String index;
+
   @override
   Widget build(BuildContext context) {
-    final index = context.currentBeamLocation.state.pathParameters['index'];
     return Scaffold(
       appBar: AppBar(title: Text('Detail for $index')),
-      body: Center(child: Text('INDEX: $index')),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('INDEX: $index'),
+            SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {
+                context.beamToNamed(
+                  '/final',
+                  popBeamLocationOnPop: true,
+                  beamBackOnPop: true,
+                );
+              },
+              child: Text('to final page'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FinalScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Final Screen')),
+      body: Center(child: Text('Can only go back from here.')),
     );
   }
 }
